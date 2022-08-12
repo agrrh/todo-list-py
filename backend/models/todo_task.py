9bca50168d8204698bd5040f0fa6d63009661bcb
Kirill K <kovalev.kirill.a@gmail.com>
2022-08-12 23:04:27 +0300

    fix(back): title must not be empty

--- backend/models/todo_task.py
+++ backend/models/todo_task.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import Optional
+from pydantic import validator
 
 from redis_om import HashModel
 
@@ -9,3 +10,9 @@ class TodoTask(HashModel):
     details: Optional[str]
     created_at: Optional[datetime]
     resolved_at: Optional[datetime] = 0
+
+    @validator("title")
+    def title_must_not_be_empty(cls, v):
+        if v.strip() == "":
+            raise ValueError("Must not be empty")
+        return v
