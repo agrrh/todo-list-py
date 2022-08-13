d37758bb877a298a0129431a454c0b8c9c1c9dc3
Kirill K <kovalev.kirill.a@gmail.com>
2022-08-13 14:47:30 +0300

    feat(back): add dynamic ttl percentage param

--- backend/models/todo_task.py
+++ backend/models/todo_task.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 from typing import Optional
 from pydantic import validator
@@ -10,6 +11,15 @@ class TodoTask(HashModel):
     details: Optional[str]
     created_at: Optional[datetime]
     resolved_at: Optional[datetime] = datetime.fromtimestamp(0)
+    ttl_left_percentage: float = 0.0
+
+    @validator("ttl_left_percentage", always=True)
+    def ttl_percentage_calculation(cls, v, values, **kwargs):
+        EXPIRE_TTL = 60 * 5
+
+        value = (time.time() - values.get("resolved_at").timestamp()) / EXPIRE_TTL
+
+        return (1 - round(value, 4)) * 100
 
     @validator("title")
     def title_must_not_be_empty(cls, v):
