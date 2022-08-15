ab6a84a4e361a407eb4f1ae909b8c47bf03ee153
Kirill K <kovalev.kirill.a@gmail.com>
2022-08-16 02:54:28 +0300

    fix(front): call env var

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,1 @@
-export const api = process.env.APP_ENV === 'local' ? "http://localhost:8081" : "http://todo-back.agrrh.com"
+export const api = import.meta.env.API_URL === 'local' ? "http://localhost:8081" : "http://todo-back.agrrh.com"
