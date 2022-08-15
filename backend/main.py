b12dcbd552d9e2ce6d3988b312d95f45df63a613
Kirill K <kovalev.kirill.a@gmail.com>
2022-08-16 02:56:00 +0300

    fix(back): cors url

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ origins = [
     "http://localhost:5173",
     "http://localhost:8080",
     "http://localhost:8081",
-    "http://front.agrrh.tech",
+    "http://todo.agrrh.com",
 ]
 
 app.add_middleware(
